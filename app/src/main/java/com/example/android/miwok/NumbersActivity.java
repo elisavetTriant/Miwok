7b7b727132071af7092fa593269b796a983bbb83
Elisavet Triantafyllopoulou <elitriant@gmail.com>
2018-03-11 01:51:40 +0200

    Miwok App V.2.01
    Adding the English number words as a String Array into the
    NumbersActivity

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -2,6 +2,7 @@ package com.example.android.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class NumbersActivity extends AppCompatActivity {
 
@@ -9,5 +10,29 @@ public class NumbersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
+
+        String [] words = new String[10];
+        words[0] = "One";
+        words[1] = "Two";
+        words[2] = "Three";
+        words[3] = "Four";
+        words[4] = "Five";
+        words[5] = "Six";
+        words[6] = "Seven";
+        words[7] = "Eight";
+        words[8] = "Nine";
+        words[9] = "Ten";
+
+        Log.v("NumbersActivity", "Word at index 0: " + words[0]);
+        Log.v("NumbersActivity", "Word at index 1: " + words[1]);
+        Log.v("NumbersActivity", "Word at index 2: " + words[2]);
+        Log.v("NumbersActivity", "Word at index 3: " + words[3]);
+        Log.v("NumbersActivity", "Word at index 4: " + words[4]);
+        Log.v("NumbersActivity", "Word at index 5: " + words[5]);
+        Log.v("NumbersActivity", "Word at index 6: " + words[6]);
+        Log.v("NumbersActivity", "Word at index 7: " + words[7]);
+        Log.v("NumbersActivity", "Word at index 8: " + words[8]);
+        Log.v("NumbersActivity", "Word at index 9: " + words[9]);
+
     }
 }
